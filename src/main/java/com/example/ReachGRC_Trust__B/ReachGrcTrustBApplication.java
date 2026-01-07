4991d8d67aaf47ce9593578ee3f9d761382d87d5
aksharkh <aksharkh04@gmail.com>
2026-01-07 12:23:16 +0530

    initialized the project

---
+++ src/main/java/com/example/ReachGRC_Trust__B/ReachGrcTrustBApplication.java
@@ -0,0 +1,13 @@
+package com.example.ReachGRC_Trust__B;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReachGrcTrustBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReachGrcTrustBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ReachGRC_Trust__B/ReachGrcTrustBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ReachGRC_Trust__B;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReachGrcTrustBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
